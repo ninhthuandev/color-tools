72ef725a1c87ffaea74cdf0abdfaccb197210b69
ninhthuandev <ninhthuan.dev@gmail.com>
2024-03-14 23:11:26 +0700

    COLOR | Init project with Color pickers tools

---
+++ js/index.js
@@ -0,0 +1,12 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.store('theme', {
+        value: 'light',
+        setTheme(themeValue) {
+            this.value = themeValue;
+            localStorage.setItem('theme', this.value);
+        },
+        toggle() {
+            this.setTheme(this.value === 'dark' ? 'light' : 'dark');
+        }
+    });
+});
